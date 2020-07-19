e0da9042ce727e097055d2b09aaf1e439fc455ee
Kyle Zsembery <zsembery.kyle@gmail.com>
2020-07-19 14:45:56 +1000

    more clean up

--- src/main/resources/db/migration/V2__InsertInitialData.sql
+++ src/main/resources/db/migration/V2__InsertInitialData.sql
@@ -6,4 +6,4 @@ INSERT INTO commission_rate
  (uuid_generate_v4(), 'Poor', 0.8, 1.0, 0.8, 1.0),
  (uuid_generate_v4(), 'OK', 1.0, 2.0, 1, 1.5),
  (uuid_generate_v4(), 'Good', 2.0, 3.0, 2.5, 0.5),
- (uuid_generate_v4(), 'God', 3.0, 99.99, 3.0, 3.0);
+ (uuid_generate_v4(), 'Zig Ziglar', 3.0, 99.99, 3.0, 3.0);
